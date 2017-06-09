dfa7b421ea0a7cc28104163c460b3ee0f74757fe
kulshekhar <kulshekhar@users.noreply.github.com>
2017-06-09 16:33:12 +0530

    Create Movie class

---
+++ media.py
@@ -0,0 +1,16 @@
+class Movie():
+    '''
+    The representation of a movie
+    '''
+
+    def __init__(self, title, storyline, trailer_youtube_url, poster_image_url):
+        '''
+        :param title: Movie title.
+        :param storyline: Movie plot.
+        :param youtube_link: url to the youtube trailer
+        :param poster_image_url: url to the movie image
+        '''
+        self.title = title
+        self.storyline = storyline
+        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_image_url
