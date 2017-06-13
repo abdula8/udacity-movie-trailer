fb1453c4dcf2d47a31743f78acb99f85d699e54d
kulshekhar <kulshekhar@users.noreply.github.com>
2017-06-13 17:49:16 +0530

    Use list comprehension & argument unpacking to create the movie list succinctly

--- movie_list.py
+++ movie_list.py
@@ -10,15 +10,5 @@ def get_movie_list():
     but can be updated later to fetch from a remote endpoint
     '''
 
-    movie_list = []
     with open('movie_list.json', 'r') as json_file:
-        movie_list_json = json.load(json_file)
-        for movie_json in movie_list_json:
-            movie_list.append(Movie(
-                title=movie_json['title'],
-                storyline=movie_json['storyline'],
-                trailer_youtube_url=movie_json['trailer_youtube_url'],
-                poster_image_url=movie_json['poster_image_url'],
-            ))
-
-    return movie_list
+        return [Movie(**i) for i in json.load(json_file)]
